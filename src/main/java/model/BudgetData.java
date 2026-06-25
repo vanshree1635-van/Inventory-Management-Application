ddce828883c397cef7b237d0526459d410d55467
Vanshree <vanshree1635@gmail.com>
2026-06-25 20:24:42 +0530

    Update BudgetData.java

--- src/main/java/model/BudgetData.java
+++ src/main/java/model/BudgetData.java
@@ -9,30 +9,37 @@ public class BudgetData {
     private final double remainingBalance;
     private final Map<String, Double> categoryDistribution;
 
+    private final double prevTotalBudget;
+    private final double prevAmountSpent;
+    private final double prevRemainingBalance;
+    private final Map<String, Double> prevCategoryDistribution;
+
     public BudgetData(double totalBudget,
                       double amountSpent,
                       double remainingBalance,
-                      Map<String, Double> categoryDistribution) {
+                      Map<String, Double> categoryDistribution,
+                      double prevTotalBudget,
+                      double prevAmountSpent,
+                      double prevRemainingBalance,
+                      Map<String, Double> prevCategoryDistribution) {
 
-        this.totalBudget = totalBudget;
-        this.amountSpent = amountSpent;
-        this.remainingBalance = remainingBalance;
-        this.categoryDistribution = categoryDistribution;
+        this.totalBudget              = totalBudget;
+        this.amountSpent              = amountSpent;
+        this.remainingBalance         = remainingBalance;
+        this.categoryDistribution     = categoryDistribution;
+        this.prevTotalBudget          = prevTotalBudget;
+        this.prevAmountSpent          = prevAmountSpent;
+        this.prevRemainingBalance     = prevRemainingBalance;
+        this.prevCategoryDistribution = prevCategoryDistribution;
     }
 
-    public double getTotalBudget() {
-        return totalBudget;
-    }
+    public double getTotalBudget()                          { return totalBudget; }
+    public double getAmountSpent()                          { return amountSpent; }
+    public double getRemainingBalance()                     { return remainingBalance; }
+    public Map<String, Double> getCategoryDistribution()    { return categoryDistribution; }
 
-    public double getAmountSpent() {
-        return amountSpent;
-    }
-
-    public double getRemainingBalance() {
-        return remainingBalance;
-    }
-
-    public Map<String, Double> getCategoryDistribution() {
-        return categoryDistribution;
-    }
+    public double getPrevTotalBudget()                      { return prevTotalBudget; }
+    public double getPrevAmountSpent()                      { return prevAmountSpent; }
+    public double getPrevRemainingBalance()                 { return prevRemainingBalance; }
+    public Map<String, Double> getPrevCategoryDistribution(){ return prevCategoryDistribution; }
 }
